7a35aa2641b5c10d21b7359d45f54999d6f7d2f2
Luis Porras <69703390+lporrasccancce@users.noreply.github.com>
2020-08-16 11:11:53 -0500

    Add files via upload

---
+++ CurrencyConverterForns/FrmCurrency.cs
@@ -0,0 +1,39 @@
+﻿using CurrencyConverterLibreria;
+using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace CurrencyConverterForms
+{
+    public partial class FrmCurrency : Form
+    {
+        public FrmCurrency()
+        {
+            InitializeComponent();
+        }
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            decimal converted = 0;
+            decimal initial = 0;
+            BaseCurrency fromCur;
+            BaseCurrency toCur;
+
+            initial = Convert.ToDecimal(Amount.Text);
+            fromCur = fromCombo.SelectedItem as BaseCurrency;
+            toCur = toCombo.SelectedItem as BaseCurrency;
+            converted = ConvertibleCurrency.CurrencyConvert(initial, fromCur, toCur);
+            Result.Text = converted.ToString();
+        }
+
+        private void FrmCurrency_Load(object sender, EventArgs e)
+        {
+            ArrayList currencyList = new ArrayList();
+
+            currencyList.Add(new PenCurrency());
+            currencyList.Add(new UsdCurrency());
+            currencyList.Add(new EurCurrency());
+            fromCombo.DataSource = currencyList;
+            toCombo.DataSource = currencyList.Clone();
+        }
+    }
+}
